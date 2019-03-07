04125d6111a54cc68ded068cc56757117e4d81a9
BeanDefintion <1529303484@qq.com>
2019-03-08 00:56:01 +0800

    swagger2的扫描也要借助springboot自身的扫描机制 所以这里把启动类的位子放进了文件夹里面 它都不扫描了 所以不管怎样配置swagger都显示的是默认的 而且 jpa默认扫描dao resposity的是enable注解 扫描 entity的是 entityscan注解 请注意

--- base/src/main/java/com/god/base/BaseApplication.java
+++ base/src/main/java/com/god/base/BaseApplication.java
@@ -2,7 +2,9 @@ package com.god.base;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import util.IdWorker;
@@ -11,8 +13,10 @@ import util.IdWorker;
  * 启动类
  */
 @EnableSwagger2
-@EnableJpaRepositories(value = "com.god.pojo")
+@EntityScan(basePackages = {"com.god.pojo"})
+@EnableJpaRepositories(value = {"com.god.dao"})
 @SpringBootApplication
+@ComponentScan(basePackages={"com"})
 public class BaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BaseApplication.class);
